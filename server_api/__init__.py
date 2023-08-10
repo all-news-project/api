84a8e7b373178f486e649d23dbf83881b39732d7
kggold4 <kggold4@gmail.com>
2023-08-10 16:22:38 +0300

    * Use cors again for API

--- server_api/__init__.py
+++ server_api/__init__.py
@@ -7,9 +7,9 @@ app.config['SECRET_KEY'] = str(uuid.uuid4())
 
 from server_api import routes
 
-# try:
-#     from flask_cors import CORS, cross_origin
-# except ImportError:
-#     pass
-#
-# CORS(app)
+try:
+    from flask_cors import CORS, cross_origin
+except ImportError:
+    pass
+
+CORS(app)
